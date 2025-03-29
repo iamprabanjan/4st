8488947175d90d483c07e9f9fcc7cedf9a335ac9
iamprabanjan <prabansundar@gmail.com>
2025-03-29 19:43:00 +0530

    Add files via upload

---
+++ while loop assignment .py
@@ -0,0 +1,32 @@
+#while loop
+
+#Addition of 1 to 50
+
+add=0
+count=0
+while count<=50:
+  add += count
+  count+=1
+  print(add)
+
+
+#Count no of word in the given input
+word=input("Enter the number to count the digit ")
+digit=0
+i=0
+while i<len(word):
+  digit =+ 1
+i+=1
+print(f"The no of digit is: {digit}")
+
+
+#Factorial
+
+userinput =input("Enter a number to find the factorial")
+n=int(userinput)
+factorial=1
+i=1
+while i<=n:
+  factorial=1
+  i=1
+  print(factorial)
